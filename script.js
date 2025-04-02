97eca4d0dad5be96d8197c504a9ec2ed47456bc3
Hermann <188955756+4483-s@users.noreply.github.com>
2025-04-02 10:55:47 +0800

    finish base functionality

---
+++ script.js
@@ -0,0 +1,20 @@
+const taskName = document.querySelector("input");
+const button = document.querySelector("button");
+const container = document.querySelector(".container");
+button.addEventListener("click", () => {
+  addTask(taskName.value);
+});
+function addTask(value) {
+  const taskDiv = document.createElement("div");
+
+  const task = document.createElement("p");
+  const removeTaskBtn = document.createElement("button");
+  task.style.display = "inline";
+  task.textContent = value;
+  removeTaskBtn.textContent = "Delete Task";
+  removeTaskBtn.addEventListener("click", () => {
+    taskDiv.remove();
+  });
+  taskDiv.append(task, removeTaskBtn);
+  container.append(taskDiv);
+}
