11220ad0e187677f8ed456fee7ecf0d06cd7ef36
Julius <188955756+4483-s@users.noreply.github.com>
2025-04-02 16:10:48 +0800

    add more basic functionality

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ button.addEventListener("click", () => {
 });
 function addTask(value) {
   const taskDiv = document.createElement("div");
-
   const task = document.createElement("p");
   const removeTaskBtn = document.createElement("button");
   const upBtn = document.createElement("button");
@@ -17,6 +16,20 @@ function addTask(value) {
   removeTaskBtn.addEventListener("click", () => {
     taskDiv.remove();
   });
-  taskDiv.append(task, removeTaskBtn);
+  upBtn.addEventListener("click", () => {
+    if (taskDiv.previousElementSibling) {
+      container.insertBefore(taskDiv, taskDiv.previousElementSibling);
+    }
+  });
+  downBtn.addEventListener("click", () => {
+    if (taskDiv.nextElementSibling) {
+      container.insertBefore(taskDiv.nextElementSibling, taskDiv);
+    }
+  });
+  upBtn.textContent = "up";
+  downBtn.textContent = "down";
+
+  taskDiv.append(task, removeTaskBtn, upBtn, downBtn);
   container.append(taskDiv);
+  taskName.value = "";
 }
